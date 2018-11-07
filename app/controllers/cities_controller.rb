ea0a28579d98f1fcaedb0843d77acdfd28c83b54
Neneh <nbojang@spartaglobal.com>
2018-11-07 17:02:40 +0000

    V.0.4 Showing the Cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,18 +1,68 @@
 class CitiesController < ApplicationController
+
+  def index
+    @cities = City.all
+  end
+
+  # GET /countries/1
+  # GET /countries/1.json
+  def show
+    @city = City.find(params[:id])
+  end
+
+  # GET /countries/new
+  def new
+    @city = City.new
+  end
+
+  # GET /countries/1/edit
+  def edit
+      @city = City.find(params[:id])
+  end
+
+  # POST /countries
+  # POST /countries.json
   def create
-  @country = Country.find(params[:user_id])
-  @city = @country.cities.create(city_params)
-  redirect_to country_path(@country)
-end
+    @country = Country.find(params[:user_id])
+    @city = @country.cities.create(city_params)
+    redirect_to country_path(@country)
+  end
 
-def destroy
-  @country = Country.find(params[:user_id])
-  @city = @country.cities.find(params[:id])
-  @city.destroy
-  redirect_to country_path(@country)
-end
+  # PATCH/PUT /countries/1
+  # PATCH/PUT /countries/1.json
+  def update
+    respond_to do |format|
+      if @city.update(city_params)
+        format.html { redirect_to @city, notice: 'City was successfully updated.' }
+        format.json { render :show, status: :ok, location: @city }
+      else
+        format.html { render :edit }
+        format.json { render json: @city.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
-private def city_params
+  # DELETE /countries/1
+  # DELETE /countries/1.json
+  def destroy
+    @city.destroy
+    respond_to do |format|
+      format.html { redirect_to cities_url, notice: 'City was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_country
+      @country = Country.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+
+
+
+def city_params
   params.require(:city).permit(:name, :population, :desciption, :country_id)
 end
 end
